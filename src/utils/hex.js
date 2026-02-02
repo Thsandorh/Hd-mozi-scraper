68f035992d85ae7106d768e7b1069a22384f055f
Thsandorh <redrick555@gmail.com>
2026-02-02 22:23:50 +0100

    Delete src directory

--- src/utils/hex.js
+++
@@ -1,8 +0,0 @@
-function hexToBytes(hex) {
-  const clean = (hex || '').trim();
-  const matches = clean.match(/[\da-f]{2}/gi);
-  if (!matches) return new Uint8Array();
-  return new Uint8Array(matches.map(byte => parseInt(byte, 16)));
-}
-
-module.exports = { hexToBytes };
